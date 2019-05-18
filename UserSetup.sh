8e3b87cc8d16705243c55b1454bf481ccafbad77
cyberrumor <46626673+cyberrumor@users.noreply.github.com>
2019-05-17 18:41:18 -0600

    Automatically runs wal to avoid issues.

--- UserSetup.sh
+++ UserSetup.sh
@@ -1,7 +1,10 @@
+echo 'Updating user directories so the copy goes smooth.'
+xdg-user-dirs-update
 echo 'Copying user configuration files to your user folder.'
 cp -r home/cyberrumor/* ~/
 echo 'Setting permissions so swaybar launches.'
 chmod +x ~/.config/swaybar/swaybar.sh
-echo 'You must run
+echo 'Setting wallpaper with wal'
+wal -i ~/Pictures/LesSquares.png
+echo 'To change your wallpaper, and theme your system dynamically, run
 	$ wal -i /path/to/image.jpg'
-echo 'to complete installation, without which there will be errors.'
