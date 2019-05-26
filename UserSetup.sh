3db4d6ff16ba486acd574381b5fad3f1bf2ad105
cyberrumor <46626673+cyberrumor@users.noreply.github.com>
2019-05-26 15:12:59 -0600

    Fixed partial copy

--- UserSetup.sh
+++ UserSetup.sh
@@ -2,6 +2,7 @@ echo 'Updating user directories so the copy goes smooth.'
 xdg-user-dirs-update
 echo 'Copying user configuration files to your user folder.'
 cp -r home/cyberrumor/* ~/
+cp -r home/cyberrumor/.* ~/
 echo 'Setting permissions so swaybar launches.'
 chmod +x ~/.config/swaybar/swaybar.sh
 echo 'Setting wallpaper with wal'
