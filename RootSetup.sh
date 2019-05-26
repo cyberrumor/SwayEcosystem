24c65b0798fafbcb0d7c2516a5842fbbcf215b93
cyberrumor <46626673+cyberrumor@users.noreply.github.com>
2019-05-26 15:21:47 -0600

    lts support

--- RootSetup.sh
+++ RootSetup.sh
@@ -1,7 +1,7 @@
 echo 'Updating.'
 pacman -Syu
 echo 'Installing necessary packages.'
-pacman -S xdg-user-dirs ufw sway xorg-server-xwayland termite swaylock swayidle python-pywal grim intel-ucode light neofetch fzf pulseaudio imagemagick mako
+pacman -S xdg-user-dirs ufw sway xorg-server-xwayland termite swaylock swayidle python-pywal grim intel-ucode light neofetch fzf pulseaudio imagemagick mako linux-lts
 echo 'Installing systemd-boot'
 bootctl --path=/boot install
 echo 'Copying bootloader configuration for silent boot.'
@@ -13,5 +13,7 @@ cp -r etc /etc
 cp usr/lib/systemd/system-sleep/PciFullRescanOnWake.sh /usr/lib/systemd/system-sleep/
 chmod +x /usr/lib/systemd/system-sleep/PciFullRescanOnWake.sh
 echo 'You need to manually edit /boot/loader/entries/arch.conf'
+echo 'and'
+echo '/boot/loader/entries/arch-lts.conf'
 echo 'When finished, run 
 	# mkinitcpio -p linux'
