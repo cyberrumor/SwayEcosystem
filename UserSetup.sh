1c691fed29d56d25d29b8404b937266e4a2c9b1d
cyberrumor <46626673+cyberrumor@users.noreply.github.com>
2019-06-02 11:07:31 -0600

    removed swaybar
    added reference to the manual dependency requirement.

--- UserSetup.sh
+++ UserSetup.sh
@@ -3,9 +3,10 @@ xdg-user-dirs-update
 echo 'Copying user configuration files to your user folder.'
 cp -r home/cyberrumor/* ~/
 cp -r home/cyberrumor/.* ~/
-echo 'Setting permissions so swaybar launches.'
-chmod +x ~/.config/swaybar/swaybar.sh
 echo 'Setting wallpaper with wal'
-wal -i ~/Pictures/LesSquares.png
+wal -i /usr/share/backgrounds/archlinux/archlinux-burn.jpg
 echo 'To change your wallpaper, and theme your system dynamically, run
 	$ wal -i /path/to/image.jpg'
+echo 'For waybar icons to display, you must install
+	the git version of material design icons from here:
+	https://aur.archlinux.org/ttf-material-design-icons-git/'
