6b7071f3069d728ffafd87152424cdffb54fb94b
cyberrumor <46626673+cyberrumor@users.noreply.github.com>
2019-06-23 15:50:26 -0600

    revert BT, add ssh + firewall
    bluez is buggy with current kernel and my hardware. Added openssh and libnotify to pacman command. Configured firewall.

--- RootSetup.sh
+++ RootSetup.sh
@@ -1,7 +1,7 @@
 echo 'Updating.'
 pacman -Syu
 echo 'Installing necessary packages.'
-pacman -S xdg-user-dirs ufw sway swaybg xorg-server-xwayland termite swaylock swayidle python-pywal grim intel-ucode light neofetch fzf pulseaudio pulseaudio-alsa imagemagick mako bemenu waybar ttf-ubuntu-font-family bluez bluez-utils archlinux-wallpaper linux-lts
+pacman -S xdg-user-dirs ufw sway swaybg xorg-server-xwayland termite swaylock swayidle python-pywal grim intel-ucode light neofetch fzf pulseaudio pulseaudio-alsa imagemagick mako libnotify openssh bemenu waybar ttf-ubuntu-font-family archlinux-wallpaper linux-lts
 echo 'Installing systemd-boot'
 bootctl --path=/boot install
 echo 'Copying bootloader configuration for silent boot.'
@@ -12,12 +12,9 @@ mkdir /etc/systemd/system
 cp -r etc /etc
 cp usr/lib/systemd/system-sleep/PciFullRescanOnWake.sh /usr/lib/systemd/system-sleep/
 chmod +x /usr/lib/systemd/system-sleep/PciFullRescanOnWake.sh
-echo 'Configuring bluetooth to allow Xbox One X and Xbox One S controllers to connect wirelessly.'
-bash -c "echo 1 > /sys/module/bluetooth/parameters/disable_ertm"
-echo 'Enabling bluetooth.service.'
-systemctl enable bluetooth.service
-echo 'Setting bluetooth to power on automatically at boot.'
-echo "AutoEnable=true" >> /etc/bluetooth/main.conf
+echo 'Configuring firewall to allow limited tcp traffic over port 22 (useful for ssh).'
+ufw default deny
+ufw limit 22/tcp
 echo 'Enabling ufw.service.'
 ufw enable
 echo 'You need to manually edit /boot/loader/entries/arch.conf'
