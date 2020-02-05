359d878327ec09bd2b1f554b34979f70a47eefa0
cyberrumor <46626673+cyberrumor@users.noreply.github.com>
2020-02-05 16:02:38 -0700

    slimmed, added completion + command-not-found

--- RootSetup.sh
+++ RootSetup.sh
@@ -1,11 +1,7 @@
 echo 'Updating.'
 pacman -Syu
 echo 'Installing necessary packages.'
-pacman -S xdg-user-dirs ufw sway swaybg xorg-server-xwayland termite swaylock swayidle python-pywal grim intel-ucode neofetch fzf pulseaudio pulseaudio-alsa pamixer imagemagick mako libnotify openssh bemenu waybar ttf-ubuntu-font-family archlinux-wallpaper linux-lts
-echo 'Installing systemd-boot'
-bootctl --path=/boot install
-echo 'Copying bootloader configuration for silent boot.'
-cp -r boot/loader/* /boot/loader/
+pacman -S xdg-user-dirs sway swaybg xorg-server-xwayland termite swaylock swayidle python-pywal grim neofetch pulseaudio pulseaudio-alsa pamixer imagemagick mako libnotify bemenu waybar ttf-ubuntu-font-family archlinux-wallpaper bash-completion pkgfile
 echo 'Copying systemwide configurations for quality of life improvements.'
 mkdir /etc/pacman.d/hooks
 mkdir /etc/systemd/system
@@ -15,14 +11,5 @@ chmod +x /usr/lib/systemd/system-sleep/PciFullRescanOnWake.sh
 mkdir /usr/local/bin
 cp usr/local/bin/* /usr/local/bin/
 chmod +x /usr/local/bin/*
-echo 'Configuring firewall to allow limited tcp traffic over port 22 (useful for ssh).'
-ufw default deny
-ufw limit 22/tcp
-echo 'Enabling ufw.service.'
-ufw enable
-echo 'You need to manually edit /boot/loader/entries/arch.conf'
-echo 'and'
-echo '/boot/loader/entries/arch-lts.conf'
-echo 'When finished, run 
-	# mkinitcpio -p linux'
-echo 'If brightness controls don't work, try adding your user to the video group.'
+echo "if you're in chroot, don't forget to install a bootloader!"
+echo "If brightness controls don't work, try adding your user to the video group."
