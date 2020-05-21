f3b902fa4ede4c08e142974acea9c97e60d74725
cyberrumor <cyberrumor@gmail.com>
2020-05-20 18:03:55 -0600

    cleaned up

--- RootSetup.sh
+++ RootSetup.sh
@@ -3,7 +3,6 @@ pacman -Syu
 echo 'Installing necessary packages.'
 pacman -S xdg-user-dirs sway swaybg xorg-server-xwayland termite swaylock swayidle python-pywal grim neofetch pulseaudio pulseaudio-alsa pamixer imagemagick mako libnotify bemenu waybar capitaine-cursors arc-gtk-theme arc-icon-theme ttf-ubuntu-font-family archlinux-wallpaper bash-completion pkgfile
 echo 'Copying systemwide configurations for quality of life improvements.'
-mkdir /etc/pacman.d/hooks
 mkdir /etc/systemd/system
 cp -r etc /etc
 cp usr/lib/systemd/system-sleep/PciFullRescanOnWake.sh /usr/lib/systemd/system-sleep/
