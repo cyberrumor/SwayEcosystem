7375bcc09e1236fa5bf09c556a7b5991eb175961
cyberrumor <cyberrumor@icloud.com>
2021-05-14 11:29:02 -0600

    cleaned up

--- RootSetup.sh
+++ RootSetup.sh
@@ -1,7 +1,7 @@
 echo 'Updating.'
 pacman -Syu
 echo 'Installing necessary packages.'
-pacman -S xdg-user-dirs sway swaybg xorg-server-xwayland alacritty swaylock swayidle python-pywal grim neofetch pipewire pipewire-pulse pamixer imagemagick mako libnotify bemenu waybar capitaine-cursors arc-gtk-theme arc-icon-theme terminus-font archlinux-wallpaper
+pacman -S xdg-user-dirs sway swaybg xorg-xwayland alacritty swaylock swayidle python-pywal grim neofetch pipewire pipewire-pulse pamixer imagemagick mako libnotify bemenu waybar capitaine-cursors arc-gtk-theme arc-icon-theme terminus-font archlinux-wallpaper
 echo 'Copying systemwide configurations for quality of life improvements.'
 mkdir /usr/local/bin
 cp usr/local/bin/* /usr/local/bin/
