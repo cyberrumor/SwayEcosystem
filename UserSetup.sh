465bb47642e131af78a797a341eddc20b96c7323
cyberrumor <cyberrumor@icloud.com>
2021-05-15 14:38:06 -0600

    added auto tiling

--- UserSetup.sh
+++ UserSetup.sh
@@ -8,5 +8,12 @@ chmod +x ~/.config/mako/launch.sh
 echo 'Setting wallpaper with wal'
 wal -i /usr/share/backgrounds/archlinux/archlinux-burn.jpg
 ln -s ~/.cache/wal/mako.conf ~/.config/mako/config
+echo 'Installing autotiling for bspwm behavior'
+mkdir ~/Builds
+cd ~/Builds
+git clone https://aur.archlinux.org/autotiling.git
+cd autotiling
+makepkg -si
+
 echo 'To change your wallpaper (which will theme your system to match), run
 	$ wal -i /path/to/image.jpg'
--- home/cyberrumor/.bash_profile
+++ home/cyberrumor/.bash_profile
@@ -4,6 +4,7 @@
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
+
 if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
 	clear && XKB_DEFAULT_LAYOUT=us XCURSOR_THEME=capitaine-cursors exec sway &> /dev/null
 fi
